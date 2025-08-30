ec0edc493c06fc46883b8b007a7e89e7d8ca821c
Sango <sangnguyen.itp@gmail.com>
2025-08-30 19:20:10 +0700

    update script

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+set -e
+
+echo "Stopping existing containers..."
+docker compose down
+echo "[v] Stopped existing containers"
+
 echo "Building custom connect image..."
 docker build -t debezium/custom-connect:latest .
 echo "[v] Image: debezium/custom-connect:latest"
